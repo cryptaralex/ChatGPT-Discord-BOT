822017bc5a800d8189c3c6759fa690efc8b7b34d
cryptaralex <34349928+cryptaralex@users.noreply.github.com>
2023-06-24 12:13:25 -0700

    regex caused problems on deployment

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ async function main() {
           
         
           try {
-            const regex = /![Image][([^[]]*?)(?=\s|]])/
+            const regex = /!\[Image\]\[([^[\]\s]*?)/;
             const prompt = 'midjrny-v4 style,' + response.text.match(regex)[1];
           
             console.log(prompt);
